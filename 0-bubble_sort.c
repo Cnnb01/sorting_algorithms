08b40dc7ff25d6897f5cb5a522c65870c7312e34
Cnnb01 <nyamambanyaguch@gmail.com>
2024-01-23 04:10:54 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,27 +9,26 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp, count = 0, swap;
-    do
-    {
-        swap = 0;
-    for (i = 0; i < size - 1; i++)
-    {
-        if(array[i] > array[i+1])
-        {
-            temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
-            swap = 1;
-            print_array(array, size);
-            count++;
-        }
-        else
-        {
-            print_array(array, size);
-            count++;
-        }
-    }
-}while(swap);
+size_t i;
+int temp, count = 0, swap;
+do{
+swap = 0;
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+temp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = temp;
+swap = 1;
+print_array(array, size);
+count++;
+}
+else
+{
+print_array(array, size);
+count++;
+}
+}
+} while (swap);
 }
